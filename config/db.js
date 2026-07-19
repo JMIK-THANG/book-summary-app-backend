6d24bc83c88cf9e9c914308e413ef2d6285acd25
Jmik Thang <jmikthang5@gmail.com>
2026-07-18 23:11:31 -0400

    google login fix

--- config/db.js
+++ config/db.js
@@ -7,8 +7,6 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl:{rejectUnauthorized:false}
+  // ssl: { rejectUnauthorized: true },
 });
-module.exports=pool
-
-
+module.exports = pool;
--- middleware/validationResult.js
+++ middleware/validationResult.js
@@ -1,10 +1,11 @@
 const { validationResult } = require("express-validator");
 
 const validateResult = (req, res, next) => {
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
     return res.status(400).json({ status: "Failed", errors: errors.array() });
   }
+  next();
 };
 
 module.exports = validateResult;
